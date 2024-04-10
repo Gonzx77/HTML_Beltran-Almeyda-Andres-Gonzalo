400fccbcb054e0aae87fac5d0107d5f48487bf7a
Gonzx77 <160796511+Gonzx77@users.noreply.github.com>
2024-04-10 17:46:24 -0500

    Formulario terminado.

---
+++ Dia5/Formulario/JS/main.js
@@ -0,0 +1,9 @@
+document.getElementById("guardarDatos").addEventListener("click", function() {
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var mensaje = document.getElementById("mensaje").value;
+
+    alert(nombre)
+    alert(email)
+    alert(mensaje)
+});
